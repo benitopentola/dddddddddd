6f732e204b27e436d6825bf8e5b18be789a96797
benitopentola <javiergraciafn@gmail.com>
2022-07-03 01:15:44 +0200

    j

--- main.py
+++ main.py
@@ -32,3 +32,32 @@ print('Creo')
 program = 'a = 555\nb=9595\nprint("Sum =", a+b*3)'
 exec(program)
 print('Ejecuta metodos creados de manera dinamica')
+
+
+#pruebas filter
+print('PRUEBA FILTER')
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Devuelve TRUE si el numero es par.
+# Number tan solo es una variable dentro de esto.
+def check_even(number):
+    if number % 2 == 0:
+          return True
+    return False
+# Extrae los numeros que sean true con el filter.
+even_numbers_iterator = filter(check_even, numbers)
+
+even_numbers = list(even_numbers_iterator)
+
+print(even_numbers)
+
+#PRUEBAS FLOAT
+
+numero222 = 25
+
+float_number = float(numero222)
+
+print(float_number)
+
+# Output: 25.0
+
